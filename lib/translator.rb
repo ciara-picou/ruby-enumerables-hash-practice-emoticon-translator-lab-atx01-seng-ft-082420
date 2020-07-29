c1562643783a307da54216ac0b02fcd0f0d26374
Ciara Picou <ciarapicou@att.net>
2020-07-29 22:52:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,12 +5,12 @@ def load_library(path)
 $emoticon_hash = {}
   emoticons = YAML.load_file(path)
   emoticons.each do |emoticon_name, emoticon_array| 
-    emoticon_hash[emoticon_name] = {
+    $emoticon_hash[emoticon_name] = {
   english: emoticon_array[0] ,
   japanese: emoticon_array[1]
 }
     end
-emoticon_hash
+$emoticon_hash
   end
 
 def get_japanese_emoticon(path, western_emoticon)
