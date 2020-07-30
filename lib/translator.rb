4ace36565278ca4e5e6bed0186e5530d1a22a86c
Ciara Picou <ciarapicou@att.net>
2020-07-30 00:05:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,7 @@ def get_japanese_emoticon(path, western_emoticon)
   
   $emoticon_hash.map do |emoticon_name, language_hash|
     binding.pry
-      if language_hash.values == western_emoticon
+      if language_hash.values[0] == western_emoticon
       return language_hash.values[1]
      end
   end
@@ -33,7 +33,7 @@ def get_english_meaning(path, japanese_emoticon)
   load_library(path)
   
   $emoticon_hash.map do |emoticon_name, language_hash|
-      if language_hash.values == japanese_emoticon
+      if language_hash.values[1] == japanese_emoticon
       return emoticon_name
         end
       end
